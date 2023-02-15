e324548a8ce9812dd35be1560ed7a84072d9a1f9
ravi <rvkn09@gmail.com>
2023-02-15 08:09:42 -0500

    Comitted application

---
+++ src/main/java/com/java/myapiv1/MyApiV1Application.java
@@ -0,0 +1,13 @@
+package com.java.myapiv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/myapiv1/MyApiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.myapiv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
